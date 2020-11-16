73d100f4a71472b53dd9c4777044f7644572a525
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-11-16 15:40:00 -0300

    Alterando funcionalidade

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # static-site-nextjs
+
+Prova de conceito de site com server rendering (NetxJS)
+
+Hospedado no Vercel, no endereço:
+
+```https://static-site-nextjs.vercel.app/```
--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,42 @@ import { useState } from "react";
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Contador />
+      <h1></h1>
+      <Lex />
     </div>
   );
 }
 
-function Contador() {
-  const [counter, setCounter] = useState(1);
+function Lex() {
+  const [showLex, setShowLex] = useState(false);
 
-  function addCounter() {
-    setCounter(counter + 1);
+  function toggleShowLex() {
+    setShowLex(!showLex);
+  }
+
+  const lexImage = (
+    <div>
+      <div>
+        <h2>Aqui está o tesão! kek</h2>
+        <img
+          src="https://i.ibb.co/M9ZpRCW/Screenshot-from-2020-11-16-15-31-00.png"
+          alt="73540"
+          border="1"
+        />
+      </div>
+      <br />
+      <button onClick={toggleShowLex}>Esconder o 73540</button>
+    </div>
+  );
+
+  if (showLex === true) {
+    return lexImage;
   }
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={addCounter}>Add</button>
+      <h2>Quer ver o 73540?</h2>
+      <button onClick={toggleShowLex}>Clique aqui</button>
     </div>
   );
 }
